a4978a08cbdddc057db6b697c0621c93568613d5
Fabian Reinders <fabian@fabiancdng.com>
2021-12-07 20:16:25 +0100

    🏷️ Add More Game Attributes & Types

--- src/game.ts
+++ src/game.ts
@@ -15,11 +15,62 @@ class Game {
     public ctx: CanvasRenderingContext2D;
 
     /**
+     * The number of rows the whole game has.
+     */
+    public rows: number;
+
+    /**
+     * The number of columns the whole game has.
+     */
+    public columns: number;
+
+    /**
+     * Position of the snake in the game / on the
+     * canvas.
+     */
+    public snake: Position;
+
+    /**
+     * Position of the food in the game / on the
+     * canvas.
+     */
+    public food: Position;
+
+    /**
+     * Width of a single cell.
+     */
+    public cellWidth: number;
+
+    /**
+     * Height of a single cell.
+     */
+    public cellHeight: number;
+
+    /**
+     * Direction the snake is heading in.
+     */
+    public direction: Direction;
+
+    /**
      * Constructor to create a new instance of the game.
      */
     public constructor() {
+        // Get canvas and its 2D context.
         this.canvas = <HTMLCanvasElement> document.getElementById('snake-canvas');
         this.ctx = <CanvasRenderingContext2D> this.canvas.getContext('2d');
+        
+        // Set initial positions of snake and food.
+        this.snake = { x: 2, y: 3 };
+        this.food = { x: 7, y: 9 };
+
+        // Organize canvas as a "grid".
+        this.rows = 10;
+        this.columns = 10;
+        this.cellWidth = this.canvas.width / this.columns;
+        this.cellHeight = this.canvas.height / this.rows;
+        
+        // Set initial direction of the snake.
+        this.direction = Direction.LEFT;
     };
 
     /**
---
+++ src/types/position.ts
@@ -0,0 +1,11 @@
+interface Position {
+    /**
+     * Position on the x-axis of the game / canvas.
+     */
+    x: number,
+
+    /**
+     * Position on the y-axis of the game / canvas.
+     */
+    y: number
+}
